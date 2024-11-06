c0e21e9fc0b5bb4d455e60ce84dc2300722d9ca5
tkThek <gabriel.takeuchinav@post.runi.ac.il>
2024-11-06 19:08:24 +0200

    first submission (before lecture 2)

--- Bill3.java
+++ Bill3.java
@@ -3,10 +3,10 @@ public class Bill3 {
 	public static void main(String[] args) {
 
 		//Stores the name of each diner and the total bill price as string and double variables.
-		double billPrice = (double) Integer.valueOf(args[0]);
-		String p1 = args[1];
-		String p2 = args[2];
-		String p3 = args[3];
+		double billPrice = (double) Integer.valueOf(args[3]);
+		String p1 = args[0];
+		String p2 = args[1];
+		String p3 = args[2];
 
 		//Rounds the divided price for each person
 		double forEach = Math.ceil(billPrice/3);
